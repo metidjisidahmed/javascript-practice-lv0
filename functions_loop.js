11190c6e7f3206f523196a80f7f811ef618c18f3
unknown <is_metidji@esi.dz>
2022-10-15 17:57:33 +0100

    loops for final version

--- functions_loop.js
+++ functions_loop.js
@@ -22,3 +22,47 @@ function divisbleNumbersByN(){
     }
     console.log("Number of multiplyers of "+ divider + " is : "+compteur)
 }
+
+function factorial(){
+    let number=Number(prompt("Give me a number to calculate its factorial"))
+    let result =1
+    for(let i = 2 ; i<= number ; i=i+1 ){
+        console.log("i = "+ i )
+        result = result*i
+        console.log("result = "+ result)
+    }
+    console.log("result = "+ result)
+
+
+}
+
+
+
+function factorialBackward(){
+    let number=Number(prompt("Give me a number to calculate its factorial"))
+    let result =1
+    for(let i = number ; i>= 2 ; i=i-1 ){
+        console.log("i = "+ i )
+        result = result*i
+        console.log("result = "+ result)
+    }
+}
+
+
+function verifyIsPrimalNumber(){
+    let number= Number(prompt("Give me the number to check whether it's primal or not"))
+    let compteur=0
+    // for(let i = number; i>= 1 ; i=i-1) {
+        for(let i = 1 ; i<= number ; i=i+1) {
+            if(number%i ==0){
+                compteur = compteur+1
+                console.log(i+ " divides "+ number)
+            }
+        }
+    alert("compteur = "+compteur)
+    if(compteur !=2){
+        alert("He is not primal")
+    }else{
+        alert("he is primal")
+    }
+}
